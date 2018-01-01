916c4ca2b4ba2d451d2c1024d3f97ca2cbceff5e
Martin Malloy <stradled@gmail.com>
2018-01-01 19:17:03 +0000

    Initial commit

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Since this component is simple and static, there's no parent container for it.
+class AboutPage extends React.Component{
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This application useds.......</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
